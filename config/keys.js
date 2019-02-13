02819fced34dd4a69fc0425c575cb0d21a1ece9c
can <mustuserefcan@gmail.com>
2019-02-13 06:07:12 +0300

    some add

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,9 @@ module.exports = {
     mongodb: {
         URI: 'mongodb://localhost:27017/player_finder_db'
     },
+    encryption: {
+        salt_for_password: '0cd1e9E3d1bD079a'
+    },
     token_key: {
         secret: 'Hm9bQeThWmZq4t7w'
     },
