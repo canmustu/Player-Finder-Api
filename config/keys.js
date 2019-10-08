6c0d7c79a810ba92cd9ef94cc71d8a43cf6ce636
Şeref Can Muştu <mustuserefcan@gmail.com>
2019-10-08 11:45:30 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongodb: {
         // URI: 'mongodb://localhost:27017/player_finder_db'
-        URI: 'mongodb://canmustu:3569466can@ds149596.mlab.com:49596/player_finder_db'
+        URI: '??'
     },
     encryption: {
         salt_for_password: '0cd1e9E3d1bD079a'
@@ -10,15 +10,15 @@ module.exports = {
         secret: 'Hm9bQeThWmZq4t7w'
     },
     google: {
-        clientID: '339914601400-ikk60dkpd5r2bmn93dbr3kunpddk1t9s.apps.googleusercontent.com',
-        clientSecret: 'JpH2bVo3g95PyA9OJzReZ0hV'
+        clientID: '??',
+        clientSecret: '??'
     },
     facebook: {
-        appID: '1944720785835404',
-        appSecret: '5fc2387a5622ecc2296a93a5945c78bc'
+        appID: '??',
+        appSecret: '??'
     },
     steam: {
-        apiKey: '8E91974DBD8A1F0DCE262C827D56BCD6',
+        apiKey: '??',
     },
     website_url: 'http://localhost:4200/',
     // api_url: 'http://localhost'
